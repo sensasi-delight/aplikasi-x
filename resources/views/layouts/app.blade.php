64ac9974aeabb18d2b488687fa16f017675ee6c4
Zain Adam <zainadam.id@gmail.com>
2023-03-11 01:45:25 +0700

    memperbarui background

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -15,7 +15,7 @@
         @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
     <body class="font-sans antialiased">
-        <div class="min-h-screen bg-gray-100 dark:bg-gray-900">
+        <div class="min-h-screen bg-gray-100 dark:bg-gray-900" style="background-image: url('{{ Vite::asset('resources/images/bg.jpg') }}'); background-size: cover; background-attachment: fixed;">
             @include('layouts.navigation')
 
             <!-- Page Heading -->
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -22,7 +22,7 @@
         </style>
     </head>
     <body class="antialiased">
-        <div class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center py-4 sm:pt-0">
+        <div class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center py-4 sm:pt-0" style="background-image: url('{{ Vite::asset('resources/images/bg.jpg') }}'); background-size: cover; background-attachment: fixed;">
             @if (Route::has('login'))
                 <div class="hidden fixed top-0 right-0 px-6 py-4 sm:block">
                     @auth
