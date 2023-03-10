5e86dc8033b15021fc0537ea1ce516611a97a71e
Zain Adam <zainadam.id@gmail.com>
2023-03-10 10:48:50 +0700

    chore: kustomisasi templat

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -22,6 +22,8 @@
                 </a>
             </div>
 
+            <h1 style="color: #EF3B2D">{{ config('app.name', 'Laravel') }}</h1>
+
             <div class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white dark:bg-gray-800 shadow-md overflow-hidden sm:rounded-lg">
                 {{ $slot }}
             </div>
